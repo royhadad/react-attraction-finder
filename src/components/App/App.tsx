59b0f6fe29649e848c57b9f6ac5084523419ef24
royhadad98 <roy@roundtrip.co.il>
2021-07-07 17:33:17 +0300

    added more logic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,51 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import styles from './styles';
 import Location from "../../types/Location";
+import Attraction from "../../types/Attraction";
+import {GetAttractionsBody} from "../../../server/apiRoutes";
+import {getAttractionsSortedByDistanceFromUserLocation} from "../../utils/attractionsUtils";
+
+function getLocation(callback) {
+    if (navigator.geolocation) {
+        var lat_lng = navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(position);
+            var user_position = {};
+            user_position.lat = position.coords.latitude;
+            user_position.lng = position.coords.longitude;
+            callback(user_position);
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
 
 const App: React.FC = () => {
     const classes = styles();
 
     const [userLocation, setUserLocation] = useState<Location | undefined>(undefined);
+    const [attractions, setAttractions] = useState<Attraction[] | undefined>(undefined);
 
     const showUserLocation = (): void => {
-
+        getLocation((position) => {
+            console.log('position', position)
+            setUserLocation(position);
+        })
     }
 
-    const showNearestAttractions = (): void => {
 
+    const showNearestAttractions = async (): Promise<void> => {
+        const response = await axios.get<GetAttractionsBody>('api/getAttractions');
+        const unorderedAttractions = response?.data?.list;
+        if (!unorderedAttractions) {
+            console.error("couldn't fetch the attractions list!");
+        } else if (!userLocation) {
+            console.error('user location was not set!')
+            return;
+        } else {
+            const orderedAttractions = getAttractionsSortedByDistanceFromUserLocation(userLocation, unorderedAttractions);
+            setAttractions(orderedAttractions)
+        }
     };
 
     return (
@@ -29,6 +62,17 @@ const App: React.FC = () => {
                         <button onClick={showNearestAttractions}>
                             Show nearest attractions!
                         </button>
+                        {
+                            attractions && (
+                                <div>
+                                    {attractions.map((attraction) => (
+                                        <div>
+                                            {JSON.stringify(attraction)}
+                                        </div>
+                                    ))}
+                                </div>
+                            )
+                        }
                     </div>
                 )}
             </div>
